2ba191dc366cda2ece53898d4944481695a7a2c5
bruno13palhano <bruno13palhano@gmail.com>
2024-12-21 19:59:14 -0300

    Implement HomePresenter

--- app/src/main/java/com/bruno13palhano/mvi/ui/screens/HomePresenter.kt
+++ app/src/main/java/com/bruno13palhano/mvi/ui/screens/HomePresenter.kt
@@ -1,49 +1,18 @@
 package com.bruno13palhano.mvi.ui.screens
 
-import com.bruno13palhano.mvi.Reducer
+import com.bruno13palhano.mvi.BasePresenter
+import com.bruno13palhano.mvi.repository.Repository
+import com.bruno13palhano.mvi.ui.di.IOScope
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.FlowCollector
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.stateIn
-import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-internal class HomePresenter(
-    private val reducer: Reducer<HomeState, HomeEvent, HomeSideEffect>,
-    private val state: HomeState,
-    private val events: Flow<HomeEvent>,
-    private val sendEvent: (event: HomeEvent) -> Unit,
-    private val sendEffect: (effect: HomeSideEffect) -> Unit,
-    private val scope: CoroutineScope
-) {
-    fun presenter(): StateFlow<HomeState> {
-        val state = MutableStateFlow(state)
-
-        return flow {
-            FlowCollector<HomeState> {
-                handleEvents(previousState = state)
-
-                emit(state.value)
-            }
-        }.stateIn(
-            scope = scope,
-            started = SharingStarted.WhileSubscribed(5_000),
-            initialValue = state.value
-        )
-    }
-
-    private suspend fun handleEvents(
-        previousState: MutableStateFlow<HomeState>
-    ) {
-        events.collect { event ->
-            val (newState, sideEffect) = reducer.reduce(previousState.value, event)
-            previousState.value = newState
-            sideEffect?.let { sendEffect(it) }
-        }
-    }
-
-}
+internal class HomePresenter @Inject constructor(
+   initialState: HomeState,
+    repository: Repository,
+    @IOScope private val ioScope: CoroutineScope
+) : BasePresenter<HomeAction, HomeEvent, HomeState, HomeSideEffect>(
+    initialState = initialState,
+    actionProcessor = HomeActionProcessor(repository = repository),
+    reducer = HomeReducer(),
+    scope = ioScope
+)
