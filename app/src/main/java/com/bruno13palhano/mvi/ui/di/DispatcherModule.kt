9c9fe703c78d338605e07e13c216648237546d18
bruno13palhano <bruno13palhano@gmail.com>
2024-12-21 19:51:10 -0300

    Add hilt module for Dispatchers DI

---
+++ app/src/main/java/com/bruno13palhano/mvi/ui/di/DispatcherModule.kt
@@ -0,0 +1,29 @@
+package com.bruno13palhano.mvi.ui.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import javax.inject.Qualifier
+
+@Qualifier
+internal annotation class Dispatcher(val dispatcher: MVIDispatchers)
+
+@Module
+@InstallIn(SingletonComponent::class)
+internal object DispatcherModule {
+    @Provides
+    @Dispatcher(MVIDispatchers.IO)
+    fun provideIODispatcher(): CoroutineDispatcher = Dispatchers.IO
+
+    @Provides
+    @Dispatcher(MVIDispatchers.MAIN)
+    fun provideMainDispatcher(): CoroutineDispatcher = Dispatchers.Main
+}
+
+enum class MVIDispatchers {
+    IO,
+    MAIN
+}
