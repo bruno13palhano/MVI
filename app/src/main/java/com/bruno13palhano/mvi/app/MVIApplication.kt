7a933977ee068ab1b984fd8b653e6b51cf6b5461
bruno13palhano <bruno13palhano@gmail.com>
2024-12-23 19:59:51 -0300

    Inject WorkManger

--- app/src/main/java/com/bruno13palhano/mvi/app/MVIApplication.kt
+++ app/src/main/java/com/bruno13palhano/mvi/app/MVIApplication.kt
@@ -1,7 +1,19 @@
 package com.bruno13palhano.mvi.app
 
 import android.app.Application
+import androidx.hilt.work.HiltWorkerFactory
+import androidx.work.Configuration
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
 @HiltAndroidApp
-class MVIApplication : Application()
+internal class MVIApplication : Application(), Configuration.Provider {
+    @Inject
+    lateinit var workerFactory: HiltWorkerFactory
+
+    override val workManagerConfiguration: Configuration
+        get() = Configuration
+            .Builder()
+            .setWorkerFactory(workerFactory)
+            .build()
+}
