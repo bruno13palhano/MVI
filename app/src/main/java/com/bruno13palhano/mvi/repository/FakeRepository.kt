218580245dd30116bfae3d1e1ca45c9757863ce1
bruno13palhano <bruno13palhano@gmail.com>
2024-12-27 18:20:39 -0300

    Refactor

--- app/src/main/java/com/bruno13palhano/mvi/repository/FakeRepository.kt
+++ app/src/main/java/com/bruno13palhano/mvi/repository/FakeRepository.kt
@@ -1,30 +1,45 @@
 package com.bruno13palhano.mvi.repository
 
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
+import com.bruno13palhano.mvi.api.ApiService
+import com.bruno13palhano.mvi.ui.di.Dispatcher
+import com.bruno13palhano.mvi.ui.di.FakeRemoteDatasource
+import com.bruno13palhano.mvi.ui.di.MVIDispatchers.DEFAULT
+import com.bruno13palhano.mvi.ui.di.MVIDispatchers.IO
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.async
+import kotlinx.coroutines.cancelChildren
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.selects.select
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
-internal class FakeRepository @Inject constructor(): Repository {
-    private val texts = mutableListOf(
-        "Text 1",
-        "Text 2",
-        "Text 3",
-        "Text 4",
-        "Text 5",
-        "Text 6",
-        "Text 7",
-        "Text 8"
-    )
-
+internal class FakeRepository @Inject constructor(
+    @FakeRemoteDatasource private val apiService: ApiService,
+    @Dispatcher(IO) private val ioDispatcher: CoroutineDispatcher,
+    @Dispatcher(DEFAULT) private val defaultDispatcher: CoroutineDispatcher
+): Repository {
     override suspend fun insertText(text: String) {
-        texts.add(text)
+        apiService.insertText(text)
     }
 
-    override suspend fun getTexts(): List<String> {
-        return CoroutineScope(Dispatchers.IO).run {
-            delay(3000)
-            texts
-        }
+    override suspend fun getTexts(): List<String> = withContext(ioDispatcher) {
+        val fakeData1 = async { apiService.getTexts1() }
+
+        val fakeData2 = async { apiService.getTexts2() }
+
+        fakeData1.await() + fakeData2.await()
+    }
+
+    override suspend fun getFastTexts(): List<String> = coroutineScope {
+        select {
+            async { apiService.getTexts1() }.onAwait { it }
+            async { apiService.getTexts2() }.onAwait { it }
+        }.also { coroutineContext.cancelChildren() }
+    }
+
+    override suspend fun fakeWork(): Boolean = withContext(defaultDispatcher) {
+        delay(5000)
+        true
     }
 }
--- app/src/main/java/com/bruno13palhano/mvi/repository/Repository.kt
+++ app/src/main/java/com/bruno13palhano/mvi/repository/Repository.kt
@@ -3,4 +3,6 @@ package com.bruno13palhano.mvi.repository
 internal interface Repository {
     suspend fun insertText(text: String)
     suspend fun getTexts(): List<String>
+    suspend fun getFastTexts(): List<String>
+    suspend fun fakeWork(): Boolean
 }
