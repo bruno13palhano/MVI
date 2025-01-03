ddc5716b5a1a33df64132556480fa50964f9db7c
bruno13palhano <bruno13palhano@gmail.com>
2025-01-03 18:47:19 -0300

    Ad Container for mvi implementation

---
+++ mvi/src/main/java/com/bruno13palhano/mvi/Container.kt
@@ -0,0 +1,46 @@
+package com.bruno13palhano.mvi
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.newSingleThreadContext
+import kotlinx.coroutines.withContext
+
+class Container<STATE, EFFECT>(
+    initialState: STATE,
+    private val scope: CoroutineScope
+) {
+    private val _state: MutableStateFlow<STATE> = MutableStateFlow(initialState)
+    val state: StateFlow<STATE> = _state.asStateFlow()
+
+    private val _sideEffect = Channel<EFFECT>(Channel.CONFLATED)
+    val sideEffect: Flow<EFFECT> = _sideEffect.receiveAsFlow()
+
+    fun intent(transform: suspend Container<STATE, EFFECT>.() -> Unit) {
+        scope.launch(SINGLE_THREAD) {
+            this@Container.transform()
+        }
+    }
+
+    suspend fun reduce(reducer: suspend STATE.() -> STATE) {
+        withContext(SINGLE_THREAD) {
+            _state.value = _state.value.reducer()
+        }
+    }
+
+    suspend fun postSideEffect(effect: EFFECT) {
+        _sideEffect.send(effect)
+    }
+
+    companion object {
+        @OptIn(ExperimentalCoroutinesApi::class, DelicateCoroutinesApi::class)
+        private val SINGLE_THREAD = newSingleThreadContext("mvi")
+    }
+}
